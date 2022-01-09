7e1e7b1d620cd3a4d8059bd07e53af2d30ce4530
Idrissa Dabo <ididabo@gmail.com>
2022-01-09 21:46:01 +0000

    change comment

--- R/square_val.R
+++ R/square_val.R
@@ -8,6 +8,6 @@
 #' @examples
 #' square_val(2)
 square_val <- function(x) {
-  stopifnot("Input must be numeric" = is.numeric(x))
+  stopifnot("Input must be numeric try again" = is.numeric(x))
   x^2
 }
